210e52ebd002ff2d2f9df013004c06a5333c1888
Rodrigo <rad-dias@hotmail.com>
2026-01-27 20:02:36 +0100

    miniature

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,7 @@ export const viewport: Viewport = {
 };
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://rodiziorace.mechama.eu"),
   title: "Rodízio Race",
   description:
     "Compete com amigos em rodízios e veja quem come mais em tempo real",
